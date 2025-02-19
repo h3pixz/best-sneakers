9ff4c5e424397d312398f70f3dcbaead5dc20153
Vladislav Rymarchik <vladrymarchik@gmail.com>
2025-02-19 21:09:17 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
        
           <div className="card">
             <div className="d-flex align-center flex-column">
-              <img height={95} width={196} src="./1.png" alt=""></img>
+              <img height={95} width={196} src="./1.png" alt="" className="kross"></img>
               <h5>Мужские кроссовки<br></br> Nike Air Force</h5>
             </div>
             
@@ -42,7 +42,10 @@ function App() {
                   <span>ЦЕНА:</span>
                   <h4>$115</h4>
                 </div>
-                <img src="./plus.svg" alt=""></img>
+                
+                <img src="./plus.svg" alt="plus" className="plus"></img>
+                
+                
           </div>
         </div>
       </div>
