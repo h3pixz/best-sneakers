9d2b7baea048b23d493b837b908c8b0b32f220ad
Vladislav Rymarchik <vladrymarchik@gmail.com>
2025-05-01 17:07:51 +0300

    компенент header

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 import React from 'react';
 import Card from './Card';
-
+import Header from './Header';
 function App() {
   return (
-    <div className="wrapper clear">
-      <header className="d-flex justify-between align-center">
-        <div className="d-flex align-center">
-          <img width={64} height={64} src="./logo.png" alt=""></img>
-          <h1>BEST SNEAKERS</h1>
-        </div>
-
-        <div className="d-flex">
-          <p>Главная</p>
-          <p>Каталог</p>
-          <p>О магазине</p>
-          <p>Отзывы</p>
-          
-        </div>
-
-        <ul className="d-flex align-center">
-          <li className="mr-40">
-            <img height={15} width={15} src="./love.svg" alt=""></img>
-            <span>Избранное</span>
-          </li>
-          <img width={34} height={34} src="./user.png" alt=""></img>
-        </ul>
-      </header>
-
+    <>
+      <Header />
       <div className="content">
         <h1>Все кроссовки</h1>
         <div className="d-flex justify-between">
-        <Card />
-        <Card />
-        <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          
         </div>
-        
-        
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="wrapper clear">
+            <header className="d-flex justify-between align-center">
+                <div className="d-flex align-center">
+                    <img width={64} height={64} src="./logo.png" alt=""></img>
+                    <h1>BEST SNEAKERS</h1>
+                </div>
+
+                <div className="d-flex">
+                    <p>Главная</p>
+                    <p>Каталог</p>
+                    <p>О магазине</p>
+                    <p>Отзывы</p>
+
+                </div>
+
+                <ul className="d-flex align-center">
+                    <li className="mr-40">
+                        <img height={15} width={15} src="./love.svg" alt=""></img>
+                        <span>Избранное</span>
+                    </li>
+                    <img width={34} height={34} src="./user.png" alt=""></img>
+                </ul>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
