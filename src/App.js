1c5138e36e5a3c46aa32dc2cd6bb373ee96f738c
Vladislav Rymarchik <vladrymarchik@gmail.com>
2025-06-09 17:21:56 +0300

    reactProps add

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Header from './Header';
 
 
 const arr = [
-  { name: 'Мужские кроссовки Nike Air Force', price: '$115'},
-  { name: 'Мужские кроссовки Nike Air Max 1 Premium', price: '$160'}
+  { name: 'Мужские кроссовки Nike Air Force', price: '$115', imageUrl: "./1.png"},
+  { name: 'Мужские кроссовки Nike Air Max 1 Premium', price: '$160', imageUrl: "./2.png"},
+  { name: 'Мужские кроссовки Nike Pegasus 41', price: '$140', imageUrl: "./3.png"},
+  { name: 'Мужские кроссовки Nike Air Pegasus Wave', price: '$125', imageUrl: "./4.png"}
 ];
 
 function App() {
@@ -15,11 +17,14 @@ function App() {
       <div className="content">
         <h1>Все кроссовки</h1>
         <div className="d-flex justify-between">
-          {
-            arr.map((obj) => (
-              <Card />
-            ))
-          }
+          {arr.map((obj) => (
+            <Card
+            title = {obj.name} 
+            price = {obj.price}
+            imageUrl = {obj.imageUrl}
+            onClick = {() => console.log(obj)} 
+          />
+          ))}
         </div>
       </div>
     </>
--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-function Card() {
+function Card(props) {
   return (
     <div className="card">
       <div className="d-flex align-center flex-column">
-        <img height={95} width={196} src="./1.png" alt="" className="kross"></img>
-        <h5>Мужские кроссовки<br></br> Nike Air Force</h5>
+        <img height={95} width={196} src={props.imageUrl} alt="" className="kross"></img>
+        <h5>{props.title}</h5>
       </div>
       <div className="d-flex justify-between">
         <div className="cena">
           <span>ЦЕНА:</span>
-          <h4>$115</h4>
+          <h4>{props.price}</h4>
         </div>
-        <img src="./plus.svg" alt="plus" className="plus"></img>
+        <div onClick={props.onClick}>
+          <img src="./plus.svg" alt="plus" className="plus"></img>  
+        </div>
+          
       </div>
     </div>
   );
