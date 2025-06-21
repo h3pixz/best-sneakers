ae024e0e55874cf48a4d25815467474264a5da64
Vladislav <vladrymarchik@gmail.com>
2025-06-21 17:19:47 +0300

    remove-btn from wrapper

--- src/App.js
+++ src/App.js
@@ -13,19 +13,30 @@ function App() {
   React.useEffect(() => {
     axios.get('https://68484c09ec44b9f349406c45.mockapi.io/items').then(res => {
       setItems(res.data);
-    })
+    });
+
+    //получение корзины
+    axios.get('https://68484c09ec44b9f349406c45.mockapi.io/card').then(res => {
+      setCardItems(res.data);
+    });
   }, [])
 
 
   //функция, которая возвращает массив в wrapper
+  //post -> отдать обьект, get -> забрать
   const onAddToCard = (obj) => {
-    setCardItems([...cardItems, obj])
+    axios.post('https://68484c09ec44b9f349406c45.mockapi.io/card', obj);
+    setCardItems((prev) => [...prev, obj]);
   }
-  
+
+  const onRemoveItem = (id) => {
+    axios.delete(`https://68484c09ec44b9f349406c45.mockapi.io/card/${id}`);
+    setCardItems((prev) => prev.filter(item => item.id !== id));
+  }
 
   return (
     <>
-      {cardOpened ? <Wrapper items={cardItems} onClose={() => { setCardOpened(false) }} /> : null}
+      {cardOpened ? <Wrapper items={cardItems} onClose={() => { setCardOpened(false) }} onRemove = {onRemoveItem} /> : null}
       <Header onClickCard={() => { setCardOpened(true) }} />
       <div className="content">
         <h1>Все кроссовки</h1>
