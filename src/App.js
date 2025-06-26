476491b9a447b1e7d8b8df7ddb48976ab555b8c7
Vladislav Rymarchik <vladrymarchik@gmail.com>
2025-06-26 21:15:59 +0300

    fix low bugs

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
 
   //берем все айтемы с BackEnd
   React.useEffect(() => {
-    axios.get('https://68484c09ec44b9f349406c45.mockapi.io/items').then(res => {
+    axios.get('https://68484c09ec44b9f349406c45.mockapi.io/items').then((res) => {
       setItems(res.data);
     });
 
     //получение корзины
-    axios.get('https://68484c09ec44b9f349406c45.mockapi.io/card').then(res => {
+    axios.get('https://68484c09ec44b9f349406c45.mockapi.io/card').then((res) => {
       setCardItems(res.data);
     });
   }, [])
@@ -25,25 +25,23 @@ function App() {
   //функция, которая возвращает массив в wrapper
   //post -> отдать обьект, get -> забрать
   const onAddToCard = (obj) => {
+    axios.post('https://68484c09ec44b9f349406c45.mockapi.io/card', obj);
+    setCardItems((prev) => [...prev, obj]);
+
     if(cardItems.find((item) => item.id === obj.id)) {
       setCardItems(prev => prev.filter(item => item.id !== obj.id));
     }
   }
 
   const onRemoveItem = (id) => {
-    try {
-      axios.delete(`https://60d62397943aa60017768e77.mockapi.io/card/${id}`);
-      setCardItems((prev) => prev.filter((item) => Number(item.id) !== Number(id)));
-    } catch(error) {
-      alert('Ошибка при удалении из корзины');
-      console.error(error);
-    }
+    axios.delete(`https://60d62397943aa60017768e77.mockapi.io/card/${id}`);
+    setCardItems((prev) => prev.filter((item) => item.id !== id));
   }
 
   return (
     <>
-      {cardOpened ? <Wrapper items={cardItems} onClose={() => { setCardOpened(false) }} onRemove = {onRemoveItem} /> : null}
-      <Header onClickCard={() => { setCardOpened(true) }} />
+      {cardOpened ? <Wrapper items={cardItems} onClose={() => setCardOpened(false)} onRemove = {onRemoveItem} /> : null}
+      <Header onClickCard={() => setCardOpened(true)} />
       <div className="content">
         <h1>Все кроссовки</h1>
         <div className="d-flex justify-between flex-wrap">
